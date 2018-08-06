c40f3c15e16157f21ce53ca4d5ff797e867cc2de
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-08-06 21:33:07 +0530

    updates

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ * Blocktron CLI
+ * A CLI tool to generate a blocktron instance.
+ * Written by: Sandeep Vattapparambil
+ * Email: sandeepv68@gmail.com
+ * Website: www.sandeepv.in
+ */
+
+ //imports
