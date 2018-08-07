6d54149919b3d8b62bdfbfb5a72ce2228ead2ccf
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-08-07 20:42:20 +0530

    repo urls

---
+++ config/repoUrls.js
@@ -0,0 +1,6 @@
+const URL_SCHEMA = {
+    blocktronNode: 'https://github.com/Blocktron-Project/blocktron-explorer.git',
+    blocktronExplorer: 'https://github.com/Blocktron-Project/blocktron-node.git'
+};
+
+export default URL_SCHEMA;
