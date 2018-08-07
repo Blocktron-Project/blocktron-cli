c04e78dcaa167bf9cb95511096b7fbacb4d56dcd
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-08-07 21:06:27 +0530

    updates

--- dist/app.bundle.js
+++ dist/app.bundle.js
@@ -112,9 +112,221 @@ __webpack_require__(1);
  */
 
 
-//imports
+var _blocktron = __webpack_require__(2);
 
-console.log('hello');
+var _blocktron2 = _interopRequireDefault(_blocktron);
+
+var _repoUrls = __webpack_require__(3);
+
+var _repoUrls2 = _interopRequireDefault(_repoUrls);
+
+var _async = __webpack_require__(4);
+
+var _async2 = _interopRequireDefault(_async);
+
+var _figlet = __webpack_require__(5);
+
+var _figlet2 = _interopRequireDefault(_figlet);
+
+var _figlet3 = __webpack_require__(6);
+
+var _figlet4 = _interopRequireDefault(_figlet3);
+
+var _chalk = __webpack_require__(7);
+
+var _chalk2 = _interopRequireDefault(_chalk);
+
+var _replaceInFile = __webpack_require__(8);
+
+var _replaceInFile2 = _interopRequireDefault(_replaceInFile);
+
+var _child_process = __webpack_require__(9);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * Log - console.log instance
+ */
+
+
+/**
+ * Replace-in-file - replace strings in files
+ */
+
+
+/**
+ * Figlet configuration
+ */
+
+
+/**
+ * async - utilities library for async operations
+ */
+var log = console.log;
+
+/**
+ * @name isWindows
+ * Helper function to check the current platform for Windows/Unix-ish.
+ * @returns {Boolean} - Returns true if Windows, else false.
+ */
+
+
+/**
+ * Exec command from child_process
+ * Function to Spawns a shell then executes the command within that shell, buffering any generated output. 
+ * The command string passed to the exec function is processed directly by the shell and special 
+ * characters (vary based on shell) need to be dealt with accordingly.
+ * Note: Never pass unsanitized user input to this function. 
+ * Any input containing shell metacharacters may be used to trigger arbitrary command execution.
+ */
+
+
+/**
+ * Chalk - colorful terminal logs
+ */
+
+
+/**
+ * Figlet - render ASCII graphics in terminal
+ */
+var isWindows = function isWindows() {
+  if (/^win/i.test(process.platform)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+/**
+ * @name run
+ * A Promise factory to execute an arbitrary shell command.
+ * @param {String} script - The arbitrary shell command to execute.
+ * @returns {<Promise>} - The promise object containing the stdout/stderr. 
+ */
+var run = function run(script) {
+  return new Promise(function (resolve, reject) {
+    /**
+     * If a callback function is provided, it is called with the arguments (error, stdout, stderr). 
+     * On success, error will be null. On error, error will be an instance of Error. 
+     * The error.code property will be the exit code of the child process while error.signal 
+     * will be set to the signal that terminated the process. 
+     * Any exit code other than 0 is considered to be an error.
+     * The stdout and stderr arguments passed to the callback will contain the stdout and stderr 
+     * output of the child process. By default, Node.js will decode the output as UTF-8 and pass 
+     * strings to the callback. The encoding option can be used to specify the character encoding 
+     * used to decode the stdout and stderr output. If encoding is 'buffer', or an unrecognized 
+     * character encoding, Buffer objects will be passed to the callback instead.
+     */
+    (0, _child_process.exec)(script, function (error, stdout, stderr) {
+      if (stderr) {
+        //reject promise with error
+        reject(stderr);
+      } else {
+        //resolve promise with success
+        resolve(stdout);
+      }
+    });
+  });
+};
+
+/**
+ * Bootstrap the generator after rendering the logo
+ */
+(0, _figlet2.default)(_blocktron2.default.name, _figlet4.default, function (err, data) {
+  if (err) {
+    log(_chalk2.default.red('logo rendering failed!: ', err));
+  }
+
+  /**
+   * render the logo in bold yellow and version in blue
+   */
+  log(_chalk2.default.yellow.bold(data));
+  log(_chalk2.default.blue('v' + _blocktron2.default.version));
+});
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+/**
+ * Application configuration object
+ */
+var _BtConfig = {
+  name: 'Blocktron Generator',
+  version: '1.0.0'
+};
+
+exports.default = _BtConfig;
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var URL_SCHEMA = {
+    blocktronNode: 'https://github.com/Blocktron-Project/blocktron-explorer.git',
+    blocktronExplorer: 'https://github.com/Blocktron-Project/blocktron-node.git'
+};
+
+exports.default = URL_SCHEMA;
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports) {
+
+module.exports = require("async");
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports) {
+
+module.exports = require("figlet");
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var figletConfig = {
+    font: 'Big',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+};
+exports.default = figletConfig;
+
+/***/ }),
+/* 7 */
+/***/ (function(module, exports) {
+
+module.exports = require("chalk");
+
+/***/ }),
+/* 8 */
+/***/ (function(module, exports) {
+
+module.exports = require("replace-in-file");
+
+/***/ }),
+/* 9 */
+/***/ (function(module, exports) {
+
+module.exports = require("child_process");
 
 /***/ })
 /******/ ]);
--- src/index.js
+++ src/index.js
@@ -12,6 +12,44 @@
 'use strict';
 
 import _BtConfig from '../config/blocktron';
+import URL_SCHEMA from '../config/repoUrls';
+
+/**
+ * async - utilities library for async operations
+ */
+import async from 'async';
+
+/**
+ * Figlet - render ASCII graphics in terminal
+ */
+import figlet from 'figlet';
+
+/**
+ * Figlet configuration
+ */
+import figletConfig from '../config/figlet';
+
+/**
+ * Chalk - colorful terminal logs
+ */
+import chalk from 'chalk';
+
+/**
+ * Replace-in-file - replace strings in files
+ */
+import replace from 'replace-in-file';
+
+/**
+ * Exec command from child_process
+ * Function to Spawns a shell then executes the command within that shell, buffering any generated output. 
+ * The command string passed to the exec function is processed directly by the shell and special 
+ * characters (vary based on shell) need to be dealt with accordingly.
+ * Note: Never pass unsanitized user input to this function. 
+ * Any input containing shell metacharacters may be used to trigger arbitrary command execution.
+ */
+import {
+  exec
+} from 'child_process';
 
 /**
  * Log - console.log instance
@@ -30,3 +68,50 @@ const isWindows = () => {
     return false;
   }
 }
+
+/**
+ * @name run
+ * A Promise factory to execute an arbitrary shell command.
+ * @param {String} script - The arbitrary shell command to execute.
+ * @returns {<Promise>} - The promise object containing the stdout/stderr. 
+ */
+const run = (script) => {
+  return new Promise((resolve, reject) => {
+    /**
+     * If a callback function is provided, it is called with the arguments (error, stdout, stderr). 
+     * On success, error will be null. On error, error will be an instance of Error. 
+     * The error.code property will be the exit code of the child process while error.signal 
+     * will be set to the signal that terminated the process. 
+     * Any exit code other than 0 is considered to be an error.
+     * The stdout and stderr arguments passed to the callback will contain the stdout and stderr 
+     * output of the child process. By default, Node.js will decode the output as UTF-8 and pass 
+     * strings to the callback. The encoding option can be used to specify the character encoding 
+     * used to decode the stdout and stderr output. If encoding is 'buffer', or an unrecognized 
+     * character encoding, Buffer objects will be passed to the callback instead.
+     */
+    exec(script, (error, stdout, stderr) => {
+      if (stderr) {
+        //reject promise with error
+        reject(stderr);
+      } else {
+        //resolve promise with success
+        resolve(stdout);
+      }
+    });
+  });
+}
+
+/**
+ * Bootstrap the generator after rendering the logo
+ */
+figlet(_BtConfig.name, figletConfig, (err, data) => {
+  if (err) {
+    log(chalk.red('logo rendering failed!: ', err));
+  }
+
+  /**
+   * render the logo in bold yellow and version in blue
+   */
+  log(chalk.yellow.bold(data));
+  log(chalk.blue(`v${_BtConfig.version}`));
+});
